bc4c7d041551dbb784cc5a7ad0efba4af78b0a8d
y1jun <yijun2206@gmail.com>
2025-08-13 18:43:31 +0900

    유저 dto

--- auth/src/main/java/com/news/auth/controllers/authController.java
+++ auth/src/main/java/com/news/auth/controllers/authController.java
@@ -1,12 +1,15 @@
 package com.news.auth.controllers;
 
 import com.news.auth.services.authService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.news.auth.responses.commonResponse;
+import com.news.auth.dto.userDto;
 
 @RestController
 @RequestMapping("/user")
@@ -16,9 +19,8 @@ public class authController {
     authService authService;
 
     @GetMapping("/join")
-    public String JoinUser() {
-        System.out.println("TEST");
-        return authService.join();
+    public String JoinUser(@RequestBody userDto user, HttpServletRequest request) {
+        return authService.join(user,request);
     }
 }
 //    @PostMapping("/login")
--- auth/src/main/java/com/news/auth/services/authService.java
+++ auth/src/main/java/com/news/auth/services/authService.java
@@ -1,14 +1,16 @@
 package com.news.auth.services;
 
 import com.news.auth.responses.commonResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import com.news.auth.dto.userDto;
 
 @Service
 @RequiredArgsConstructor
 public class authService {
 
-    public String join() {
+    public String join(userDto user, HttpServletRequest request) {
         return "test";
     }
 }
